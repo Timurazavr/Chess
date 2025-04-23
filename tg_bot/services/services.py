ff75669d8d8b58d033e10b097ad558fb950e0b5f
Timur <timur.garipov11@gmail.com>
2025-04-23 21:37:26 +0300

    0.1.1

--- tg_bot/services/services.py
+++ tg_bot/services/services.py
@@ -38,39 +38,101 @@ class Chess:
         if self.field[y][x]:
             return self.field[y][x].can_move(new_x, new_y, self.field)
 
+    def hod(self, x, y, new_x, new_y):
+        if self.can_move(x, y, new_x, new_y):
+            self.field[y][x], self.field[new_y][new_x] = None, self.field[y][x]
+            self.field[new_y][new_x].x, self.field[new_y][new_x].y = new_x, new_y
+
 
 class Pawn:  # Пешка
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
 
-    def can_move(self, new_x: int, new_y: int, field):
+    def __str__(self):
+        return self.__class__.__name__
+
+    def can_move(self, new_x: int, new_y: int, field: list[list]):
         if self.color == "white":
-            if obj is None:
-                return new_x == self.x and (
-                    new_y - self.y == -1 or (new_y - self.y == -2 and self.y == 6)
-                )
-            elif obj.color != self.color:
-                return abs(new_x - self.x) == 1 and new_y - self.y == -1
+            if new_x == self.x:
+                if not (self.y - new_y == 1 or (self.y - new_y == 2 and self.y == 6)):
+                    return False
+                for i in range(new_y, self.y):
+                    if field[i][new_x] is not None:
+                        return False
+            elif abs(new_x - self.x) == 1:
+                if not self.y - new_y == 1:
+                    return False
+                if (
+                    field[new_y][new_x] is None
+                    or field[new_y][new_x].color == self.color
+                ):
+                    return False
         elif self.color == "black":
-            if obj is None:
-                return new_x == self.x and (
-                    new_y - self.y == 1 or (new_y - self.y == 2 and self.y == 1)
-                )
-            elif obj.color != self.color:
-                return abs(new_x - self.x) == 1 and new_y - self.y == 1
+            if new_x == self.x:
+                if not (self.y - new_y == -1 or (self.y - new_y == -2 and self.y == 1)):
+                    return False
+                for i in range(new_y, self.y, -1):
+                    if field[i][new_x] is not None:
+                        return False
+            elif abs(new_x - self.x) == 1:
+                if not self.y - new_y == -1:
+                    return False
+                if (
+                    field[new_y][new_x] is None
+                    or field[new_y][new_x].color == self.color
+                ):
+                    return False
+        return True
 
 
 class Queen:  # Ферзь
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
 
-    def can_move(self, new_x, new_y, obj):
-        pass
+    def __str__(self):
+        return self.__class__.__name__
+
+    def can_move(self, new_x: int, new_y: int, field: list[list]):
+        if not (
+            abs(self.x - new_x) == abs(self.y - new_y)
+            or self.x == new_x
+            or self.y == new_y
+        ):
+            # Проверка может ли физически переместиться в данную клетку
+            return False
+        if field[new_y][new_x] is not None and field[new_y][new_x].color == self.color:
+            # Проверка не пытается ли съесть своих
+            return False
+        if abs(self.x - new_x) == abs(self.y - new_y):
+            # Проверка есть ли фигуры на пути к точке
+            step_x, step_y = abs(new_x - self.x) // (new_x - self.x), abs(
+                new_y - self.y
+            ) // (new_y - self.y)
+            old_x, old_y = self.x, self.y
+            while abs(old_x - new_x) != 1:
+                old_x += step_x
+                old_y += step_y
+                if field[old_y][old_x] is not None:
+                    return False
+        if self.x == new_x or self.y == new_y:
+            # Проверка есть ли фигуры на пути к точке
+            if abs(self.x - new_x) > 1:
+                for j in range(min(self.x, new_x) + 1, max(self.x, new_x)):
+                    if field[self.y][j] is not None:
+                        return False
+            if abs(self.y - new_y) > 1:
+                for i in range(min(self.y, new_y) + 1, max(self.y, new_y)):
+                    if field[i][self.x] is not None:
+                        return False
+        return True
 
 
 class King:  # Король
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
+
+    def __str__(self):
+        return self.__class__.__name__
 
     def can_move(self, new_x: int, new_y: int, field: list[list]):
         if not (abs(self.x - new_x) <= 1 and abs(self.y - new_y) <= 1):
@@ -86,6 +148,9 @@ class Officer:  # Офицер
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
 
+    def __str__(self):
+        return self.__class__.__name__
+
     def can_move(self, new_x: int, new_y: int, field: list[list]):
         if not (abs(self.x - new_x) == abs(self.y - new_y)):
             # Проверка может ли физически переместиться в данную клетку
@@ -94,15 +159,24 @@ class Officer:  # Офицер
             # Проверка не пытается ли съесть своих
             return False
         # Проверка есть ли фигуры на пути к точке
-        for i in range(min(self.y, new_y), max(self.y, new_y)):
-            for j in range(min(self.x, new_x), max(self.x, new_x)):
-                1
+        step_x, step_y = abs(new_x - self.x) // (new_x - self.x), abs(
+            new_y - self.y
+        ) // (new_y - self.y)
+        old_x, old_y = self.x, self.y
+        while abs(old_x - new_x) != 1:
+            old_x += step_x
+            old_y += step_y
+            if field[old_y][old_x] is not None:
+                return False
         return True
 
 
 class Rook:  # Ладья
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
+
+    def __str__(self):
+        return self.__class__.__name__
 
     def can_move(self, new_x: int, new_y: int, field: list[list]):
         if not (self.x == new_x or self.y == new_y):
@@ -127,6 +201,9 @@ class Knight:  # Конь
     def __init__(self, x: int, y: int, color: str):
         self.x, self.y, self.color = x, y, color
 
+    def __str__(self):
+        return self.__class__.__name__
+
     def can_move(self, new_x: int, new_y: int, field: list[list]):
         if not (
             (abs(self.x - new_x) == 1 and abs(self.y - new_y) == 2)
@@ -141,4 +218,7 @@ class Knight:  # Конь
 
 
 ch = Chess()
-print(ch.can_move(0, 0, 0, 2))
+while 1:
+    for i in ch.field:
+        print(*i)
+    ch.hod(*map(int, input().split()))

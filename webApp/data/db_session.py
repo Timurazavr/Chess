e807571903fdfb40e6e6fbaa65d9dd0a8bd7564c
pashok7290 <pashok.vit@gmail.com>
2025-05-05 23:07:31 +0300

    решена ошибка переполненности запросов

--- webApp/data/db_session.py
+++ webApp/data/db_session.py
@@ -22,7 +22,7 @@ def global_init(db_file):
     conn_str = f'sqlite:////{db_file.strip()}?check_same_thread=False&echo={ECHO}'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
+    engine = sa.create_engine(conn_str, echo=False, pool_size=20, max_overflow=20, pool_timeout=10)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
